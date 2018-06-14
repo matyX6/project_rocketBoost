b67b3f378baa3091f0c0ad2a93c921a61ddb91e0
matyX6 <matyX6@gmail.com>
2018-06-14 15:33:54 +0200

    PB25 - orbs final, and orbs scene change

--- Assets/Orb.cs
+++ Assets/Orb.cs
@@ -5,16 +5,44 @@ using UnityEngine;
 public class Orb : MonoBehaviour
 {
 
+    int rotationMode;
+
     // Use this for initialization
     void Start()
     {
-
+        rotationMode = Random.Range(1, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
+        RotateOrb(rotationMode);
+    }
 
+    private void RotateOrb(int mode)
+    {
+        switch (mode)
+        {
+            case 1:
+                transform.Rotate(Vector3.up * 1.1f);
+                transform.Rotate(Vector3.right * 1.1f);
+                break;
+
+            case 2:
+                transform.Rotate(-Vector3.up * 1.1f);
+                transform.Rotate(Vector3.right * 1.1f);
+                break;
+
+            case 3:
+                transform.Rotate(Vector3.up * 1.1f);
+                transform.Rotate(-Vector3.right * 1.1f);
+                break;
+
+            default:
+                transform.Rotate(-Vector3.up * 1.1f);
+                transform.Rotate(-Vector3.right * 1.1f);
+                break;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
